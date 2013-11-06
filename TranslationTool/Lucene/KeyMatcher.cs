f5019e995a3622a4263b282c931d6d64fd898873
dmunch <dmunch@lucca.fr>
2013-11-06 11:26:40 +0100

    Enhance KeyMatcher

--- TranslationTool/Lucene/KeyMatcher.cs
+++ TranslationTool/Lucene/KeyMatcher.cs
@@ -11,11 +11,12 @@ namespace TranslationTool.Memory
 	public class KeyMatcher
 	{
 		public Dictionary<string, string> Matches { get; protected set; }
-
+		public List<string> NoMatches { get; protected set; }
 		public KeyMatcher(TranslationProject tpBase, TranslationProject tp, string masterLanguage = null)
 		{
 			var memory = new TranslationMemory(tpBase);
 			Matches = new Dictionary<string,string>();
+			NoMatches = new List<string>();
 
 			masterLanguage = masterLanguage ?? tp.MasterLanguage;
 
@@ -23,10 +24,14 @@ namespace TranslationTool.Memory
 			{
 				var results = memory.Query(masterLanguage, seg.Value);
 
-				if(results.Any())
+				if (results.Any())
 				{
 					Matches.Add(seg.Key, results.First().Key);
-				}				
+				}
+				else
+				{
+					NoMatches.Add(seg.Key);
+				}
 			}
 		}
 
@@ -36,19 +41,31 @@ namespace TranslationTool.Memory
 		/// <param name="tp"></param>
 		public void ChangeKeys(TranslationProject tp)
 		{
+			int counter = 0;
 			foreach(var kvp in Matches)
 			{
 				if(kvp.Key == kvp.Value) continue;
 
-				foreach(var lang in tp.Languages)
+				foreach(var langKey in tp.Languages)
 				{
-					if(tp.Dicts[lang].ContainsKey(kvp.Key) && !tp.Dicts[lang].ContainsKey(kvp.Value))
+					var lang = tp.Dicts[langKey];
+
+					if (lang.ContainsKey(kvp.Key) && !lang.ContainsKey(kvp.Value))
 					{
-						tp.Dicts[lang].Add(kvp.Value, tp.Dicts[lang][kvp.Key]);
-						tp.Dicts[lang].Remove(kvp.Key);
-					} else if(!tp.Dicts[lang].ContainsKey(kvp.Key))
+						lang.Add(kvp.Value, lang[kvp.Key]);
+						lang.Remove(kvp.Key);
+						counter++;
+					}
+					else if (!lang.ContainsKey(kvp.Key))
 					{
-						tp.Dicts[lang].Add(kvp.Value, "");
+						lang.Add(kvp.Value, "");
+					}
+					else
+					{
+						//multiple matches found
+						//in this case we apply the special rule for lucca which consist in concetenating the strings
+						lang[kvp.Value] += " <br /><br />" + lang[kvp.Key];
+						lang.Remove(kvp.Key);
 					}
 				}				
 			}
