7b59bdde8715cbdfa3d4a05512678b5ddcedcc56
dmunch <dmunch@lucca.fr>
2013-11-13 17:24:46 +0100

    Refactor into MemoryWorksheet

---
+++ TranslationTool/IO/AbstractWorksheet/MemoryWorksheet.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TranslationTool.IO.AbstractWorksheet
+{
+	public class MemoryWorksheet : IWorksheet
+	{
+		public List<List<object>> Cells;
+
+		public int Rows
+		{
+			get
+			{
+				return Cells.Count;
+			}
+		}
+
+		public int Columns
+		{
+			get
+			{
+				if (Cells.Count == 0) return 0;
+
+				return Cells[0].Count;
+			}
+		}
+
+		public MemoryWorksheet()
+		{			
+			this.Cells = new List<List<object>>();
+		}
+
+		public object this[int row, int column]
+		{
+			get
+			{
+				row -= 1; column -= 1;
+
+				if (row < 0 || Cells.Count < row)
+					return null;
+
+				if (column < 0 || Cells[row].Count < column)
+					return null;
+
+				return Cells[row][column];
+			}
+			set
+			{
+				row -= 1; column -= 1;
+
+				if (Cells.Count <= row)
+					while (Cells.Count <= row)
+						Cells.Add(new List<object>());
+
+				if (Cells[row].Count <= column)
+					while (Cells[row].Count <= column)
+						Cells[row].Add(new List<object>());
+
+				Cells[row][column] = value;
+			}
+		}
+	}
+}
--- TranslationTool/IO/OpenXml.cs
+++ TranslationTool/IO/OpenXml.cs
@@ -234,65 +234,7 @@ namespace TranslationTool.IO
 			return result;
 		}
 	}
-
-	public class OpenXmlWorksheet2 : IWorksheet
-	{
-		public List<List<object>> Cells;
-
-		public int Rows
-		{
-			get
-			{
-				return Cells.Count;
-			}
-		}
-
-		public int Columns
-		{
-			get
-			{
-				if (Cells.Count == 0) return 0;
-
-				return Cells[0].Count;
-			}
-		}
-
-		public OpenXmlWorksheet2()
-		{			
-			this.Cells = new List<List<object>>();
-		}
-
-		public object this[int row, int column]
-		{
-			get
-			{
-				row -= 1; column -= 1;
-
-				if (row < 0 || Cells.Count < row)
-					return null;
-
-				if (column < 0 || Cells[row].Count < column)
-					return null;
-
-				return Cells[row][column];
-			}
-			set
-			{
-				row -= 1; column -= 1;
-
-				if (Cells.Count <= row)
-					while (Cells.Count <= row)
-						Cells.Add(new List<object>());
-
-				if (Cells[row].Count <= column)
-					while (Cells[row].Count <= column)
-						Cells[row].Add(new List<object>());
-
-				Cells[row][column] = value;
-			}
-		}
-	}
-
+	
 	class OpenXml
 	{
 		private static string ColumnLetter(int intCol)
@@ -330,7 +272,7 @@ namespace TranslationTool.IO
 		{
 			MemoryStream xlsStream = new MemoryStream();
 
-			var worksheet = new OpenXmlWorksheet2();
+			var worksheet = new AbstractWorksheet.MemoryWorksheet();
 			Export.ToIWorksheet(project, worksheet);
 
 			using (SpreadsheetDocument myDoc = SpreadsheetDocument.Create(xlsStream, SpreadsheetDocumentType.Workbook))
@@ -389,7 +331,7 @@ namespace TranslationTool.IO
 		{
 			MemoryStream xlsStream = new MemoryStream();
 
-			var worksheet = new OpenXmlWorksheet2();
+			var worksheet = new AbstractWorksheet.MemoryWorksheet();
 			Export.ToIWorksheet(project, worksheet);
 			
 			var ss = new Stylesheet();
