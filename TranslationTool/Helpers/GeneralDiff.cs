2fe777ed3124ef179d709610863d1697538b88ab
dmunch <dmunch@lucca.fr>
2013-11-19 17:27:17 +0100

    Restructure source code

---
+++ TranslationTool/Helpers/GeneralDiff.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace TranslationTool.Helpers
+{
+	public class GeneralDiff<T, TKey, TContent>
+	{
+		public Dictionary<TKey, T> Orig = new Dictionary<TKey, T>();
+		public Dictionary<TKey, T> Updated = new Dictionary<TKey, T>();
+		public Dictionary<TKey, T> New = new Dictionary<TKey, T>();
+
+		protected Func<T, TKey> KeySelector;
+		public Func<T, TContent> ContentSelector;
+		protected Action<T, TContent> ContentSetter;
+
+		//protected IEqualityComparer<TContent> EqualityComparer;
+		protected Func<TContent, TContent, bool> EqualityComparer;
+
+		public GeneralDiff(Expression<Func<T, TKey>> _KeySelector, Expression<Func<T, TContent>> contentSelector)
+		{
+			this.KeySelector = _KeySelector.Compile();
+			this.ContentSelector = contentSelector.Compile();
+
+			var newValue = Expression.Parameter(contentSelector.Body.Type);
+			var assign = Expression.Lambda<Action<T, TContent>>(
+								Expression.Assign(contentSelector.Body, newValue),
+								contentSelector.Parameters[0], newValue);
+
+			ContentSetter = assign.Compile();
+		}
+
+
+		public void Diff(IEnumerable<T> list1, IEnumerable<T> list2)
+		{
+			//Diff(new LambdaComparer<T, TKey>(KeySelector));
+			//IEqualityComparer<T> equalityComparer = null;
+			var dDiff = new DictDiff();
+
+			this.Orig.Clear();
+			this.Updated.Clear();
+			this.New.Clear();
+
+			var dict1 = list1.ToDictionary(KeySelector);
+			var dict2 = list2.ToDictionary(KeySelector);
+
+			/*
+			if (d1 == null)
+			{
+				foreach (var kvp in d2)
+				{
+					toSync.New.Add(kvp.Key, kvp.Value);				
+				}
+			}
+			*/
+			foreach (var kvp in dict1)
+			{
+				if (dict2.ContainsKey(kvp.Key) && !EqualityComparer(ContentSelector(kvp.Value), ContentSelector(dict2[kvp.Key])))
+				//if (dict2.ContainsKey(kvp.Key) && !ContentSelector(kvp.Value).Equals(ContentSelector(dict2[kvp.Key])))
+				{
+					Updated.Add(kvp.Key, dict2[kvp.Key]);
+					Orig.Add(kvp.Key, kvp.Value);
+				}
+			}
+
+			//check new keys  
+			foreach (var kvp in dict2)
+			{
+				if (!dict1.ContainsKey(kvp.Key))
+				{
+					New.Add(kvp.Key, kvp.Value);
+				}
+			}
+		}
+
+
+
+		public void Print(TextWriter os = null)
+		{
+			if (os == null)
+				os = Console.Out;
+
+			os.WriteLine("Updated {0} rows.", Updated.Count);
+			foreach (var kvp in Updated)
+			{
+				os.WriteLine("K: {0}\n Old: {1} \n New: {2}", KeySelector(kvp.Value), ContentSelector(Orig[kvp.Key]), ContentSelector(kvp.Value));
+			}
+
+			os.WriteLine("Added {0} rows.", New.Count);
+			foreach (var kvp in New)
+			{
+				os.WriteLine(kvp.Key);
+			}
+		}
+
+		public void Patch(IList<T> listToPatch, IEnumerable<T> lookup)
+		{
+			this.Patch(listToPatch, listToPatch.ToDictionary(KeySelector));
+		}
+
+		public void Patch(IList<T> listToPatch)
+		{
+			this.Patch(listToPatch, listToPatch);
+		}
+
+		public void Patch(IList<T> listToPatch, IDictionary<TKey, T> lookup)
+		{
+			var diff = this;
+
+			foreach (var kvp in diff.New)
+			{
+				if (!lookup.ContainsKey(kvp.Key))
+				{
+					listToPatch.Add(kvp.Value);
+				}
+			}
+
+			foreach (var kvp in diff.Updated)
+			{
+				ContentSetter(lookup[kvp.Key], ContentSelector(kvp.Value));
+			}
+		}
+	}
+}
---
+++ TranslationTool/Helpers/StringContentDiff.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.IO;
+
+namespace TranslationTool.Helpers
+{
+	public class StringContentDiff<T, TKey> : GeneralDiff<T, TKey, string>
+	{
+		public StringContentDiff(Expression<Func<T, TKey>> KeySelector, Expression<Func<T, string>> contentSelector)
+			: base(KeySelector, contentSelector)
+		{
+			this.EqualityComparer = (s1, s2) => s1 == s2; //&& !string.IsNullOrWhiteSpace(s1) && !string.IsNullOrWhiteSpace(s2);
+		}
+
+		public void PrintDiff(TextWriter os = null)
+		{
+			if (os == null)
+				os = Console.Out;
+
+			var diff = new DiffMatchPatch.diff_match_patch();
+			foreach (var kvp in Updated)
+			{
+				var diffs = diff.diff_main(ContentSelector(Orig[kvp.Key]), ContentSelector(kvp.Value));
+				diff.diff_cleanupSemantic(diffs);
+				os.WriteLine("K: {0}, diffs {1}", kvp.Key, diffs.Count);
+
+				foreach (var d in diffs)
+				{
+					os.WriteLine("{0}: {1}", d.operation, d.text);
+				}
+				//diff.diff_prettyHtml(diffs);
+			}
+		}
+	}
+}
