d5e9917b5ce456c27e6061af2328aac32138235e
dmunch <dmunch@lucca.fr>
2013-11-20 18:58:51 +0100

    Add TranslationProjectDiff, based on LastModifed date

---
+++ TranslationTool/Core/TranslationProjectDiff.cs
@@ -0,0 +1,24 @@
+﻿using TranslationTool.Helpers;
+
+namespace TranslationTool
+{
+	public class TranslationProjectDiff : GeneralDiff<TranslationModule, string, System.DateTime>
+	{
+		public TranslationProjectDiff()
+			: base(p => p.Name, p => p.LastModified)
+		{
+			this.IsModifiedComparer = (d1, d2) => d1 < d2;
+		}
+	}
+
+	public static class ProjectExtensionMethods
+	{
+		public static TranslationProjectDiff Diff(this ITranslationProject p, ITranslationProject other)
+		{
+			var tpd = new TranslationProjectDiff();
+
+			tpd.Diff(p.Modules, other.Modules);
+			return tpd;
+		}
+	}
+}
