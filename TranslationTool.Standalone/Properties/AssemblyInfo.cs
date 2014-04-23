7a0c18f2accc4106468a4c08fa6167fe0f159484
dmunch <dmunch@lucca.fr>
2014-04-23 17:01:14 +0200

    Call TranslationTool.Exe tt.exe and adjust version and copyright information

--- TranslationTool.Standalone/Properties/AssemblyInfo.cs
+++ TranslationTool.Standalone/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TranslationTool.Standalone")]
+[assembly: AssemblyTitle("TranslationTool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("TranslationTool.Standalone")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCompany("X")]
+[assembly: AssemblyProduct("TranslationTool")]
+[assembly: AssemblyCopyright("-")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
